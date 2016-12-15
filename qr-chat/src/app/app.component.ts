0adb609d538f8c7b2989fedc98dad108a2342c52
Mirko Bojcic <bojcicm@gmail.com>
2016-12-15 23:08:19 +0100

    moved everything to ng2 app

--- qr-chat/src/app/app.component.ts
+++ qr-chat/src/app/app.component.ts
@@ -1,29 +1,44 @@
 import { Component } from '@angular/core';
 import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
-//import {QRCodeComponent} from 'angular2-qrcode';
+ 
+// http://zyramedia.github.io/ng2-facebook-sdk/
 
-export class User{
+export class User {
   userId: string;
-  token1: string;
-  token2: string;
+  tokenData: any;
 }
 
 @Component({
   selector: 'app-root',
-  //providers: [QRCodeComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
   title = 'app works!';
+  userQrCodeUrl: string = '';
   user: FirebaseObjectObservable<User>;
+  isFacebookLoading: boolean = false;
 
   constructor(af: AngularFire) {
-    var userId = af.database.list('/').$ref.ref.child('/users').push().key;
-
+    let userId = af.database.list('/').$ref.ref.child('/users').push().key;
     this.user = af.database.object('/users/' + userId);
-
+    this.userQrCodeUrl = 'https://api.qrserver.com/v1/create-qr-code/?data=' + userId + '&size=150x150&format=svg';
     this.user.set({userId: userId});
+
+
+    let a = this.user.subscribe(
+      userEdited => userEdited.tokenData ? this.searchFacebookData(userEdited) : console.log('user removed'),
+      error => console.log("error has occured", error),
+      () => console.log("its done"));  
+  }
+
+  searchFacebookData(userData: User): void{
+    console.log(userData.tokenData);
+    this.isFacebookLoading = true;
+
+    //load Facebook data
+    //isFacebookLoading = false;
+
   }
 }
--- qr-chat/src/app/app.module.ts
+++ qr-chat/src/app/app.module.ts
@@ -3,18 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
-import { QRCodeModule } from 'angular2-qrcode';
-
-
 import { AppComponent } from './app.component';
 
 // Must export the config
 export const firebaseConfig = {
-    apiKey: "AIzaSyAxwfi_K3RSaFFxM9ZH8G_PdFODB6aJR5A",
-    authDomain: "qr-chat.firebaseapp.com",
-    databaseURL: "https://qr-chat.firebaseio.com",
-    storageBucket: "qr-chat.appspot.com",
-    messagingSenderId: "63654247789"
+    apiKey: 'AIzaSyAxwfi_K3RSaFFxM9ZH8G_PdFODB6aJR5A',
+    authDomain: 'qr-chat.firebaseapp.com',
+    databaseURL: 'https://qr-chat.firebaseio.com',
+    storageBucket: 'qr-chat.appspot.com',
+    messagingSenderId: '63654247789'
   };
 
 @NgModule({
@@ -25,8 +22,7 @@ export const firebaseConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    QRCodeModule
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
